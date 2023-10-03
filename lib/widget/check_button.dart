c009b71b43d8c2f50ba6e544793f52ef8b0b5cb7
Haneol <haneolj@gmail.com>
2023-10-03 23:58:50 +0900

    [FEAT] Implement check button widget

--- lib/widget/check_button.dart
+++ lib/widget/check_button.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CheckButton extends StatefulWidget {
-  const CheckButton({super.key});
+  bool isCheck;
+  bool isMain;
+  Color color;
+  final ValueChanged<bool> onCheckedChanged; // 콜백 함수 추가
+
+  CheckButton({
+    this.isCheck = false,
+    this.isMain = true,
+    required this.color,
+    required this.onCheckedChanged, // 콜백 함수를 생성자로 전달
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<CheckButton> createState() => _CheckButtonState();
@@ -10,7 +22,23 @@ class CheckButton extends StatefulWidget {
 class _CheckButtonState extends State<CheckButton> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          widget.isCheck = !widget.isCheck;
+          widget.onCheckedChanged(widget.isCheck); // 상태가 변경될 때 콜백 호출
+        });
+      },
+      iconSize: widget.isMain ? 24 : 20,
+      icon: widget.isCheck
+          ? Icon(
+        CupertinoIcons.check_mark_circled_solid,
+        color: widget.color,
+      )
+          : Icon(
+        CupertinoIcons.circle,
+        color: widget.color,
+      ),
+    );
   }
 }
