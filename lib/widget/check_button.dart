4bf24025248dad1009c41ea7a82363c9b2bc6d2f
Haneol <haneolj@gmail.com>
2023-10-03 17:14:02 +0900

    [INIT] Add widget directory & check_button.dart

---
+++ lib/widget/check_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CheckButton extends StatefulWidget {
+  const CheckButton({super.key});
+
+  @override
+  State<CheckButton> createState() => _CheckButtonState();
+}
+
+class _CheckButtonState extends State<CheckButton> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
