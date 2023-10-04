51dfe7ac73ca17805b46e01732e0416230381919
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-10-04 11:13:07 +0900

    [FEAT] GestureDetector added.

--- lib/widget/category_button.dart
+++ lib/widget/category_button.dart
@@ -15,47 +15,61 @@ class CategoryButton extends StatelessWidget {
     required this.emoji,
   }) : super(key: key);
 
+  // onTap 이벤트 handler
+  void handleOnTap() {
+    debugPrint("Tapped");
+  }
+
+  // onLongPress 이벤트 handler
+  void handleOnLongPress() {
+    debugPrint("Long Pressed");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 34.0,
-      decoration: BoxDecoration(
-        color: Palette.peepButton300,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                // Emoji
-                Text(emoji),
-                const SizedBox(
-                  width: 3,
-                ),
-                // Category Name
-                Text(
-                  name,
-                  style: TextStyle(
-                      color: color,
-                      fontSize: TextSize.small,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const SizedBox(
-              width: 15,
-            ),
-            // Add Icon
-            const Icon(
-              Iconsax.add_circle5,
-              color: Palette.peepYellow400,
-              size: 18,
-            )
-          ],
+    return GestureDetector(
+      onTap: handleOnTap,
+      onLongPress: handleOnLongPress,
+      child: Container(
+        height: 34.0,
+        decoration: BoxDecoration(
+          color: Palette.peepButton300,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  // Emoji
+                  Text(emoji),
+                  const SizedBox(
+                    width: 3,
+                  ),
+                  // Category Name
+                  Text(
+                    name,
+                    style: TextStyle(
+                        color: color,
+                        fontSize: TextSize.small,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              // Add Icon
+              const Icon(
+                Iconsax.add_circle5,
+                color: Palette.peepYellow400,
+                size: 18,
+              )
+            ],
+          ),
         ),
       ),
     );
