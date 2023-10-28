74d04a64512f4921c471f7bf829de75b566ea2e9
Haneol <haneolj@gmail.com>
2023-10-28 19:58:54 +0900

    [REFACTOR] modify Icon to PeepIcon and Change TodoDefault to PeepTodoBase

--- lib/app/theme/icons.dart
+++ lib/app/theme/icons.dart
@@ -31,4 +31,7 @@ class Iconsax {
   static const calendar = 'image/icon/calendar.svg';
   static const routine = 'image/icon/routine.svg';
   static const profile = 'image/icon/profile.svg';
+  static const priority = 'image/icon/priority.svg';
+  static const arrowCircleUp = 'image/icon/arrow_circle_up.svg';
+  static const arrowCircleDown = 'image/icon/arrow_circle_down.svg';
 }
---
+++ lib/app/views/common/todo/peep_todo_base.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class PeepTodoBase extends StatelessWidget {
+  const PeepTodoBase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class PeepSubTodoBaseItem extends StatelessWidget {
+  const PeepSubTodoBaseItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
