c6300baa89c32d75e6977ce12de1cdbb267e5c81
hs99 <modesty0606@gmail.com>
2023-11-04 16:53:55 +0900

    [TSK-49] fix PeepScheduledTodoAppBar widget with DropdownMenu

--- lib/app/views/main/widget/peep_scheduled_todo_app_bar.dart
+++ lib/app/views/main/widget/peep_scheduled_todo_app_bar.dart
@@ -4,7 +4,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:peep_todo_flutter/app/theme/text_style.dart';
 import 'package:peep_todo_flutter/app/views/common/buttons/peep_notification_button.dart';
+import 'package:peep_todo_flutter/app/views/common/peep_dropdown_menu.dart';
 
+import '../../../theme/app_values.dart';
 import '../../../theme/icons.dart';
 import '../../../theme/palette.dart';
 
@@ -14,14 +16,20 @@ class DateOfPick extends GetxController {
 
 class PeepScheduledTodoAppBar extends StatelessWidget {
   final String selectedDate;
-  final Function() onTapFunc; // 추가된 콜백 함수
-  final Function() onTapSecondFunc; //추가된 콜백 함수
+  final Function() onTapClock;
+  final Function() onTapFunc; // dropdown_menu 콜백 함수
+  final Function() onTapSecondFunc; //dropdown_menu 콜백 함수
+  final Function() onTapThirdFunc; //dropdown_menu 콜백 함수
+  final Function() onTapFourthFunc; //dropdown_menu 콜백 함수
 
   const PeepScheduledTodoAppBar({
     Key? key,
     required this.selectedDate,
+    required this.onTapClock,
     required this.onTapFunc,
     required this.onTapSecondFunc,
+    required this.onTapThirdFunc,
+    required this.onTapFourthFunc,
   }) : super(key: key);
 
   @override
@@ -29,7 +37,8 @@ class PeepScheduledTodoAppBar extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      padding: EdgeInsets.symmetric(horizontal: 20.w), // 옆의 여백 조절
+      padding: const EdgeInsets.symmetric(horizontal: AppValues.screenPadding),
+      // 옆의 여백 조절
       decoration: BoxDecoration(
         border: Border.all(color: Palette.peepBackground), // 박스 테두리 스타일링
         borderRadius: BorderRadius.circular(8.r), // 박스 모서리 라운딩
@@ -40,11 +49,23 @@ class PeepScheduledTodoAppBar extends StatelessWidget {
           Text(selectedDate, style: PeepTextStyle.boldXL(Palette.peepBlack)),
           Row(
             children: [
-              PeepNotificationButton(icon: PeepIcon(Iconsax.clock, size: 24.r, color: Palette.peepBlack,), isNotified: true, onTapFunc: onTapFunc,),
+              PeepNotificationButton(
+                icon: const PeepIcon(
+                  Iconsax.clock,
+                  size: AppValues.iconDefaultSize,
+                  color: Palette.peepBlack,
+                ),
+                isNotified: true,
+                onTapFunc: onTapClock,
+              ),
               SizedBox(width: 10.w), // 아이콘 사이의 간격 조절
-              IconButton(onPressed: onTapSecondFunc, icon: PeepIcon(Iconsax.more, size: 24.r, color: Palette.peepBlack,)),
+              PeepDropdownMenu(
+                onTapFunc: onTapFunc,
+                onTapSecondFunc: onTapSecondFunc,
+                onTapThirdFunc: onTapThirdFunc,
+                onTapFourthFunc: onTapFourthFunc,
+              ),
             ],
-
           ),
         ],
       ),
