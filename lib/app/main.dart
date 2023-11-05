1f9f98c8546312758035ec6ca5aab0c726b9abc6
Haneol <haneolj@gmail.com>
2023-11-06 03:42:29 +0900

    [FEAT] Modify PeepCheckButton

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         title: '삐약Todo',
         debugShowCheckedModeBanner: false,
         theme: appThemeData,
-        initialRoute: AppPages.INITIAL,
+        initialRoute: AppPages.TEST,
         getPages: AppPages.routes,
         initialBinding: InitialBinding(),
         defaultTransition: Transition.fade,
