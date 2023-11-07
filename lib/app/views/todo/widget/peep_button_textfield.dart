1c8c00a1cf73245dd012383e6920fd9b227b699c
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-11-07 17:23:47 +0900

    [FEAT] add PeepCategoryTextfield

--- lib/app/views/todo/widget/peep_button_textfield.dart
+++ lib/app/views/todo/widget/peep_button_textfield.dart
@@ -4,7 +4,11 @@ import 'package:peep_todo_flutter/app/theme/app_values.dart';
 import 'package:peep_todo_flutter/app/theme/icons.dart';
 import 'package:peep_todo_flutter/app/theme/palette.dart';
 import 'package:peep_todo_flutter/app/theme/text_style.dart';
+import 'package:peep_todo_flutter/app/views/category/widget/peep_emoji_picker_button.dart';
 
+/*
+   PeepTodoTextfield
+*/
 class PeepTodoTextfield extends StatelessWidget {
   final PeepIcon icon;
   final Color color;
@@ -41,6 +45,81 @@ class PeepTodoTextfield extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             icon,
+            // PeepEmojiPickerButton
+            SizedBox(
+              width: 230.w,
+              child: TextField(
+                controller: controller,
+                onSubmitted: handleAddButtonTap,
+                style: PeepTextStyle.boldL(color: Palette.peepBlack),
+                cursorColor: color,
+                decoration: InputDecoration(
+                  border: InputBorder.none, // 밑줄 제거
+                  hintText: '이름을 입력해 주세요',
+                  hintStyle: PeepTextStyle.boldL(color: Palette.peepGray300),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                handleAddButtonTap(controller.text);
+              },
+              child: PeepIcon(
+                Iconsax.addSquare,
+                size: AppValues.largeIconSize,
+                color: color,
+              ),
+            ),
+            // PeepColorPickerButton
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+   PeepCategoryTextfield
+*/
+class PeepCategoryTextfield extends StatelessWidget {
+  final String emoji;
+  final Color color;
+  final VoidCallback onTapEmoji;
+  final Function(String) onTapAddButton;
+
+  const PeepCategoryTextfield({
+    Key? key,
+    required this.emoji,
+    required this.color,
+    required this.onTapEmoji,
+    required this.onTapAddButton,
+  }) : super(key: key);
+
+  void handleAddButtonTap(String text) {
+    onTapAddButton(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+
+    return Container(
+      width: AppValues.screenWidth - AppValues.screenPadding * 2,
+      height: 64.h,
+      decoration: BoxDecoration(
+        color: Palette.peepGray50,
+        borderRadius: BorderRadius.circular(AppValues.baseRadius),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: AppValues.horizontalMargin),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            PeepEmojiPickerButton(
+              emoji: emoji,
+              onTap: onTapEmoji,
+            ),
             // PeepEmojiPickerButton
             SizedBox(
               width: 230.w,
