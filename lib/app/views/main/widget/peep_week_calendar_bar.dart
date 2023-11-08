77038274c0a0e364269f04ee8d17336593638ce3
hs99 <modesty0606@gmail.com>
2023-11-09 02:34:18 +0900

    [FEAT] fix peep_week_calendar & add palette.dart

--- lib/app/views/main/widget/peep_week_calendar_bar.dart
+++ lib/app/views/main/widget/peep_week_calendar_bar.dart
@@ -27,14 +27,14 @@ class ringPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final double total = 9/2;
+    final double total = 9 / 2;
 
-    final double startAngle = -pi/2;
+    final double startAngle = -pi / 2;
 
     double currentAngle = startAngle;
 
     for (int rank in itemRanks) {
-      final sweepAngle = ( (itemCounts[rank-1]/ total) * pi ); // 아이템 수에 따른 각도
+      final sweepAngle = ((itemCounts[rank - 1] / total) * pi); // 아이템 수에 따른 각도
       final rect = Rect.fromCircle(
         center: Offset(size.width / 2, size.height / 2),
         radius: size.width / 2,
@@ -57,10 +57,9 @@ class ringPainter extends CustomPainter {
   }
 }
 
-
 class PeepWeekCalendarBar extends StatelessWidget {
   final WeekCalendarController calendarController =
-  Get.put(WeekCalendarController());
+      Get.put(WeekCalendarController());
   final List<DropdownMenuItemData> dropdownMenuItems;
   final Function(String) onMenuItemSelected;
   final Function() onTapClock;
@@ -72,39 +71,49 @@ class PeepWeekCalendarBar extends StatelessWidget {
     required this.onTapClock,
   }) : super(key: key);
 
-
   Widget customDowBuilder(BuildContext context, DateTime day) {
     final text = DateFormat.E('ko_KR').format(day);
+    final isSelected = calendarController.selectedDay.value;
     final isToday = isSameDay(day, calendarController.toDay.value);
-    if (isToday) {
-      return Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Palette.peepYellow400, // 배경색 설정
-            borderRadius:
-            BorderRadius.circular(AppValues.tinyRadius), // 모서리 라운딩 설정
-          ),
-          padding: EdgeInsets.all(AppValues.innerMargin), // 텍스트 주위의 여백 설정
-          child: Text(
-            text,
-            style: PeepTextStyle.regularXS(color: Palette.peepWhite),
-          ),
-        ),
-      );
-    } else if (day.weekday == DateTime.sunday) {
-      return dayOfWeekDecorate(text, Palette.peepRed);
-    } else if (day.weekday == DateTime.saturday) {
-      return dayOfWeekDecorate(text, Palette.peepBlue);
-    } else {
-      return dayOfWeekDecorate(text, Palette.peepGray400);
-    }
-  }
-
-  Widget dayOfWeekDecorate(text, Color color) {
     return Center(
-      child: Text(
-        text,
-        style: PeepTextStyle.regularXS(color: color),
+      child: Stack(
+        children: [
+          if (isToday)
+            if (isSameDay(day, isSelected))
+              Container(
+                width: 44.w, // Container의 너비
+                height: 40.h, // Container의 높이
+                decoration: BoxDecoration(
+                  color: Palette.peepYellow50,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(AppValues.smallRadius),
+                    topRight: Radius.circular(AppValues.smallRadius),
+                  ),
+                ),
+              )
+            else
+              Container(
+                decoration: BoxDecoration(
+                  color: Palette.peepYellow400, // 배경색 설정
+                  borderRadius:
+                      BorderRadius.circular(AppValues.tinyRadius), // 모서리 라운딩 설정
+                ),
+                padding: EdgeInsets.all(AppValues.innerMargin), // 텍스트 주위의 여백 설정
+                child: Text(text,
+                    style: PeepTextStyle.boldXS(color: Palette.peepWhite)),
+              )
+          else
+            Container(
+              padding: EdgeInsets.all(AppValues.innerMargin), // 텍스트 주위의 여백 설정
+              child: Text(text,
+                  style: day.weekday == DateTime.sunday
+                      ? PeepTextStyle.regularXS(color: Palette.peepRed)
+                      : day.weekday == DateTime.saturday
+                          ? PeepTextStyle.regularXS(color: Palette.peepBlue)
+                          : PeepTextStyle.regularXS(
+                              color: Palette.peepGray400)),
+            ),
+        ],
       ),
     );
   }
@@ -123,15 +132,73 @@ class PeepWeekCalendarBar extends StatelessWidget {
       appBar: AppBar(
         title: Text('TableCalendar - Range'),
       ),
-      body: Obx(() =>
-          TableCalendar(
+      body: Obx(() => TableCalendar(
             calendarBuilders: CalendarBuilders(
+              outsideBuilder: (context, day, focusedDay) {
+                return Center(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      //CustomPaint(
+                      //  size: Size(32.w, 32.h),
+                      //  painter: ringPainter(itemRanks, itemColors, itemCounts),
+                      //),
+                      Text(
+                        DateFormat.d().format(day),
+                        style:
+                            PeepTextStyle.regularXS(color: Palette.peepGray400),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              selectedBuilder: (context, day, selectedDay) {
+                return Center(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: 44.w, // Container의 너비
+                        height: 70.h, // Container의 높이
+                        decoration: BoxDecoration(
+                          color: Palette.peepYellow50,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(AppValues.smallRadius),
+                            bottomRight: Radius.circular(AppValues.smallRadius),
+                          ),
+                        ),
+                      ),
+                      CustomPaint(
+                        size: Size(32.w, 32.h), // CustomPaint의 크기 고정
+                        painter: ringPainter(itemRanks, itemColors, itemCounts),
+                      ),
+                      Center(
+                        child: Text(
+                          DateFormat.d().format(selectedDay),
+                          style:
+                              PeepTextStyle.regularXS(color: Palette.peepBlack),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
               dowBuilder: customDowBuilder,
               defaultBuilder: (context, day, focusedDay) {
                 return Center(
-                  child: CustomPaint(
-                    size: Size(32.w, 32.h),
-                    painter: ringPainter(itemRanks, itemColors, itemCounts),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CustomPaint(
+                        size: Size(32.w, 32.h),
+                        painter: ringPainter(itemRanks, itemColors, itemCounts),
+                      ),
+                      Text(
+                        DateFormat.d().format(day),
+                        style:
+                            PeepTextStyle.regularXS(color: Palette.peepBlack),
+                      ),
+                    ],
                   ),
                 );
               },
@@ -151,7 +218,7 @@ class PeepWeekCalendarBar extends StatelessWidget {
               rightChevronVisible: false,
               headerMargin: EdgeInsets.all(2 * AppValues.horizontalMargin),
               headerPadding:
-              EdgeInsets.symmetric(vertical: AppValues.verticalMargin),
+                  EdgeInsets.symmetric(vertical: AppValues.verticalMargin),
             ),
             calendarStyle: CalendarStyle(
               defaultTextStyle: PeepTextStyleBase.baseRegularM,
@@ -161,7 +228,7 @@ class PeepWeekCalendarBar extends StatelessWidget {
                   bottomLeft: Radius.circular(AppValues.smallRadius),
                   // 좌하단 반지름 값
                   bottomRight:
-                  Radius.circular(AppValues.smallRadius), // 우하단 반지름 값
+                      Radius.circular(AppValues.smallRadius), // 우하단 반지름 값
                 ),
               ),
               outsideDecoration: BoxDecoration(
@@ -170,7 +237,7 @@ class PeepWeekCalendarBar extends StatelessWidget {
                   bottomLeft: Radius.circular(AppValues.smallRadius),
                   // 좌하단 반지름 값
                   bottomRight:
-                  Radius.circular(AppValues.smallRadius), // 우하단 반지름 값
+                      Radius.circular(AppValues.smallRadius), // 우하단 반지름 값
                 ),
                 color: null,
               ),
@@ -196,7 +263,7 @@ class PeepWeekCalendarBar extends StatelessWidget {
                   bottomLeft: Radius.circular(AppValues.smallRadius),
                   // 좌하단 반지름 값
                   bottomRight:
-                  Radius.circular(AppValues.smallRadius), // 우하단 반지름 값
+                      Radius.circular(AppValues.smallRadius), // 우하단 반지름 값
                 ),
                 color: null,
               ),
@@ -208,12 +275,12 @@ class PeepWeekCalendarBar extends StatelessWidget {
             ),
 
             daysOfWeekStyle: const DaysOfWeekStyle(
-              //weekdayStyle: TextStyle(color: P),
-              //decoration: Decoration()
+                //weekdayStyle: TextStyle(color: P),
+                //decoration: Decoration()
 
-            ),
-            daysOfWeekHeight: 30.h,
-            rowHeight: 57.h,
+                ),
+            daysOfWeekHeight: 24.h,
+            rowHeight: 60.h,
             locale: 'ko_KR',
             firstDay: DateTime.utc(2023, 1, 1),
             lastDay: DateTime.utc(2023, 12, 31),
