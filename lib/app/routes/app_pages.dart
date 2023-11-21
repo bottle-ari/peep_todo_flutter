ebb7eed7122df70d412a101d06db9d4b61e8d02b
hs99 <modesty0606@gmail.com>
2023-11-21 22:53:04 +0900

    [FEAT] add TodoDetail Page & controller & binding and add App Routes & Pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 import 'package:peep_todo_flutter/app/bindings/page/category_manage_page_binding.dart';
 import 'package:peep_todo_flutter/app/bindings/page/overdue_todo_binding.dart';
+import 'package:peep_todo_flutter/app/bindings/page/todo_detail_binding.dart';
+import 'package:peep_todo_flutter/app/bindings/page/todo_memo_binding.dart';
 import 'package:peep_todo_flutter/app/views/test.dart';
 import 'package:peep_todo_flutter/app/views/category/page/category_manage_page.dart';
 import 'package:peep_todo_flutter/app/views/todo/page/overdue_todo_page.dart';
+import 'package:peep_todo_flutter/app/views/todo/page/todo_detail_page.dart';
+import 'package:peep_todo_flutter/app/views/todo/page/todo_memo_page.dart';
 
 import '../bindings/main_binding.dart';
 import '../views/main/page/main_page.dart';
@@ -16,6 +20,8 @@ class AppPages {
   static const TEST = Routes.TEST_PAGE;
   static const INITIAL = Routes.MAIN;
   static const OVERDUETODO = Routes.OVERDUE_TODO_PAGE;
+  static const TODODETAIL = Routes.TODO_DETAIL_PAGE;
+  static const TODOMEMO = Routes.TODO_MEMO_PAGE;
 
   static final routes = [
     GetPage(
@@ -39,6 +45,18 @@ class AppPages {
       page: () => OverdueTodoPage(),
       binding: OverdueTodoBinding(),
       transition: Transition.rightToLeft,
-    )
+    ),
+    GetPage(
+      name: _Paths.TODO_DETAIL_PAGE,
+      page: () => TodoDetailPage(),
+      binding: TodoDetailBinding(),
+      transition: Transition.rightToLeft,
+    ),
+    GetPage(
+      name: _Paths.TODO_MEMO_PAGE,
+      page: () => TodoMemoPage(),
+      binding: TodoMemoBinding(),
+      transition: Transition.rightToLeft,
+    ),
   ];
 }
