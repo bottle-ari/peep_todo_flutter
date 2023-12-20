8be68bd280f666b72519cfaf2e3cde792d5bc8fe
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-12-20 19:15:53 +0900

    [FEAT] implement PeepRoutineManageListItem.

--- lib/app/views/routine/widget/peep_routine_manage_list_item.dart
+++ lib/app/views/routine/widget/peep_routine_manage_list_item.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:peep_todo_flutter/app/controllers/data/routine_controller.dart';
-import 'package:peep_todo_flutter/app/controllers/page/routine_manage_controller.dart';
 import 'package:peep_todo_flutter/app/data/model/routine/routine_model.dart';
 import 'package:peep_todo_flutter/app/theme/app_values.dart';
 import 'package:peep_todo_flutter/app/theme/icons.dart';
 import 'package:peep_todo_flutter/app/theme/palette.dart';
 import 'package:peep_todo_flutter/app/theme/text_style.dart';
 import 'package:peep_todo_flutter/app/views/common/popup/peep_confirm_popup.dart';
-
-import '../../../controllers/data/todo_controller.dart';
 import 'peep_routine_toggle_button.dart';
 
 class PeepRoutineManageListItem extends StatelessWidget {
@@ -66,53 +64,95 @@ class PeepRoutineManageListItem extends StatelessWidget {
             onTap: onTap,
             child: Container(
               width: AppValues.screenWidth - AppValues.screenPadding * 2,
-              height: AppValues.largeItemHeight,
               color:
               routine.isActive ? Palette.peepWhite : Palette.peepGray100,
-              child: Padding(
-                padding:
-                EdgeInsets.symmetric(horizontal: AppValues.horizontalMargin),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: AppValues.innerMargin,
-                        ),
-                        if(routine.isActive)
-                          Text(
-                            // Todo : text 길이 조절
-                            routine.name.length > 9
-                                ? "${routine.name.substring(0, 9)}..."
-                                : routine.name,
-                            style: PeepTextStyle.regularM(color: Palette.peepBlack),
-                          )
-                        else
-                          ColorFiltered(
-                            colorFilter: const ColorFilter.matrix([
-                              0.2126, 0.7152, 0.0722, 0, 0,
-                              0.2126, 0.7152, 0.0722, 0, 0,
-                              0.2126, 0.7152, 0.0722, 0, 0,
-                              0,      0,      0,      1, 0,
-                            ]),
-                            child: Text(
-                              // Todo : text 길이 조절
-                              routine.name.length > 9
-                                  ? "${routine.name.substring(0, 9)}..."
-                                  : routine.name,
-                              style: PeepTextStyle.regularM(color: Palette.peepBlack),
-                            ),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: AppValues.largeItemHeight),
+                child: Padding(
+                  padding:
+                  EdgeInsets.symmetric(horizontal: AppValues.horizontalMargin),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: AppValues.innerMargin,
                           ),
-                      ],
-                    ),
-                    PeepRoutineToggleButton(
-                      routine: routine,
-                      controller: controller,
-                    ),
-                  ],
+                          if(routine.isActive)
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 5.h),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 230.w,
+                                    child: Text(
+                                      routine.name,
+                                      style: PeepTextStyle.regularM(color: Palette.peepBlack),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 3,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      PeepIcon(Iconsax.routineOutline, size: 14.w, color: Palette.peepBlack,),
+                                      SizedBox(width: AppValues.innerMargin),
+                                      Text(routine.repeatCondition,
+                                        style: PeepTextStyle.regularXS(color: Palette.peepBlack),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            )
+                          else
+                            ColorFiltered(
+                              colorFilter: const ColorFilter.matrix([
+                                0.2126, 0.7152, 0.0722, 0, 0,
+                                0.2126, 0.7152, 0.0722, 0, 0,
+                                0.2126, 0.7152, 0.0722, 0, 0,
+                                0,      0,      0,      1, 0,
+                              ]),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5.h),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: 230.w,
+                                      child: Text(
+                                        routine.name,
+                                        style: PeepTextStyle.regularM(color: Palette.peepBlack),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 3,
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        PeepIcon(Iconsax.routineOutline, size: 14.w, color: Palette.peepBlack,),
+                                        SizedBox(width: AppValues.innerMargin),
+                                        Text(routine.repeatCondition,
+                                          style: PeepTextStyle.regularXS(color: Palette.peepBlack),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                      PeepRoutineToggleButton(
+                        routine: routine,
+                        controller: controller,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
